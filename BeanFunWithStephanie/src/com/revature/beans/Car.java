a3b5bec11df635a56be8ea6761b4630d06c7a27c
Stephanie Foshay <foshaystephanie@gmail.com>
2020-10-20 16:18:00 -0500

    Bean Fun

---
+++ BeanFunWithStephanie/src/com/revature/beans/Car.java
@@ -0,0 +1,10 @@
+package com.revature.beans;
+
+public class Car {
+	String model;
+	String color;
+	
+	public Car (String model, String color) {
+		System.out.println("Hi, I'm a " + model + ". I'm " + color);
+	};
+}
